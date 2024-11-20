a0f6c6f389f8a68d2e26c901e57971a315a57c97
mjr2595 <michaeljohnraymond@gmail.com>
2024-11-19 23:20:57 -0600

    Added projects page

---
+++ src/scripts/projects.js
@@ -0,0 +1,68 @@
+const projects = {
+  nuron: {
+    name: "Nuron",
+    image: {
+      url: "https://i.imgur.com/7WUeR5T.png",
+      alt: "image of Nuron app on a tablet, phone, and laptop. Caption says 'rewrite your story'",
+    },
+    links: {
+      splash: "https://thenuronway.com/",
+      live: "https://chataiden.com/",
+    },
+    description:
+      "Flutter application with a focus on self-empowerment and productivity. Written in Dart with BLoC architecture and AWS Amplify backend. Available on iOS, Android, macOS, Windows.",
+  },
+  gamehub: {
+    name: "Game Hub",
+    image: {
+      url: "https://i.imgur.com/bc9Sv9f.png",
+      alt: "Screenshot of Game Hub app showing game library with search bar and filters. Below is a grid of game cards with cover images and ratings.",
+    },
+    links: {
+      github: "https://github.com/mjr2595/game-hub",
+      live: "https://game-hub-mjr2595.vercel.app/",
+    },
+    description:
+      "Video game discovery web app leveraging the rawg.io API. Built with React, TypeScript, Chakra UI, Axios, React Query, React Router, and Zustand.",
+  },
+  jhj: {
+    name: "JessieHeartJace",
+    image: {
+      url: "https://i.imgur.com/OdAnBaV.png",
+      alt: "Screenshot of JessieHeartJace website showing homepage. Title says 'Let's spread some pawsitivity' and features a hero image with a boston terrier and lab.",
+    },
+    links: {
+      github: "https://github.com/mjr2595/pet-services",
+      live: "https://jessieheartjace.vercel.app/",
+    },
+    description:
+      "Website for my wife's pet services. Built with React, TypeScript, and Chakra UI.",
+  },
+  barkbook: {
+    name: "Barkbook",
+    image: {
+      url: "https://i.imgur.com/ldfa1QJ.png",
+      alt: "Screenshot of Barkbook social media app showing feed with posts, comments, and likes. Profile sidebar shows user avatar, name, and bio.",
+    },
+    links: {
+      github: "https://github.com/mjr2595/borker",
+      demo: "https://www.youtube.com/watch?v=cskBOrNxFP4",
+    },
+    description:
+      "Social media for dogs. MERN Stack application for Nucamp Coding Bootcamp Honors certificate.",
+  },
+  linksync: {
+    name: "Link Sync",
+    image: {
+      url: "https://i.imgur.com/sEvndG9.png",
+      alt: "Screenshot of Link Sync site showing a list of links with icons and copy button.",
+    },
+    links: {
+      github: "https://github.com/mjr2595/link-sync",
+      live: "https://mjr2595.github.io/link-sync/",
+    },
+    description: "All my links in a simple list. HTML, CSS, JS.",
+  },
+};
+
+export default projects;

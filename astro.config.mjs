194531f9262574d5437702991f1164701ffa5a81
mjr2595 <michaeljohnraymond@gmail.com>
2024-11-20 22:44:16 -0600

    Add sitemap integration

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,4 +1,16 @@
-import { defineConfig } from 'astro/config';
+import { defineConfig } from "astro/config";
+import sitemap from "@astrojs/sitemap";
+import { SITE_URL } from "./src/config";
 
 // https://astro.build/config
-export default defineConfig({});
+export default defineConfig({
+  site: SITE_URL,
+  base: "/",
+  integrations: [sitemap()],
+  markdown: {
+    shikiConfig: {
+      theme: "material-theme-darker",
+      langs: [],
+    },
+  },
+});
--- src/config.js
+++ src/config.js
@@ -1,2 +1,3 @@
 export const SITE_TITLE = "Michael Raymond (mray)";
 export const SITE_DESCRIPTION = "code. coffee. tacos. music. all at once.";
+export const SITE_URL = "https://mray-blogolio.netlify.app";
